72a341d204557c77a9df236641eba99db954bf88
shubham <shubhambansal096@gmail.com>
2024-09-29 03:21:17 +0530

    Github api integrated

--- frontend/src/components/Repos.jsx
+++ frontend/src/components/Repos.jsx
@@ -1,60 +1,21 @@
-import { FaCodeBranch, FaCopy, FaRegStar } from "react-icons/fa";
-import { FaCodeFork } from "react-icons/fa6";
+import Repo from "./Repo";
 
-const Repo = () => {
+const Repos = ({ repos, alwaysFullWidth = false }) => {
+  const className = alwaysFullWidth ? "w-full" : "lg:w-2/3 w-full";
   return (
-    <li className="mb-10 ms-7">
-      <span
-        className="absolute flex items-center justify-center w-6 h-6 bg-blue-100
-			rounded-full -start-3 ring-8 ring-white"
-      >
-        <FaCodeBranch className="w-5 h-5 text-blue-800" />
-      </span>
-      <div className="flex gap-2 items-center flex-wrap">
-        <a
-          href={"https://github.com/burakorkmez/mern-chat-app"}
-          target="_blank"
-          rel="noreferrer"
-          className="flex items-center gap-2 text-lg font-semibold"
-        >
-          mern-chat-app
-        </a>
-        <span
-          className="bg-yellow-100 text-yellow-800 text-xs font-medium px-2.5
-					py-0.5 rounded-full flex items-center gap-1"
-        >
-          <FaRegStar /> 167
-        </span>
-        <span
-          className="bg-purple-100 text-purple-800 text-xs font-medium
-					 px-2.5 py-0.5 rounded-full flex items-center gap-1"
-        >
-          <FaCodeFork /> 25
-        </span>
-        <span
-          className="cursor-pointer bg-green-100 text-green-800 text-xs
-					font-medium px-2.5 py-0.5 rounded-full flex items-center gap-1"
-        >
-          <FaCopy /> Clone
-        </span>
-      </div>
-
-      <time
-        className="block my-1 text-xs font-normal leading-none
-			 text-gray-400"
-      >
-        Released on Jan 1, 2021
-      </time>
-      <p className="mb-4 text-base font-normal text-gray-500">
-        Real Time Chat App | MERN && Socket.io && JWT
-      </p>
-      <img
-        src={"/javascript.svg"}
-        alt="Programming language icon"
-        className="h-8"
-      />
-    </li>
+    <div className={`${className} bg-glass rounded-lg px-8 py-6`}>
+      <ol className="relative border-s border-gray-200">
+        {repos.map((repo) => (
+          <Repo key={repo.id} repo={repo} />
+        ))}
+        {repos.length === 0 && (
+          <p className="flex items-center justify-center h-32">
+            No repos found
+          </p>
+        )}
+      </ol>
+    </div>
   );
 };
 
-export default Repo;
+export default Repos;
