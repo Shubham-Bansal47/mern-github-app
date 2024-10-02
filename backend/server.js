5411e500b0a9ba7e50f42ac69738ac701c88d182
shubham <shubhambansal096@gmail.com>
2024-10-02 23:15:01 +0530

    Added frontend and backend route

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,15 @@ import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
 import connectMongoDB from "./db/connectToMongoDB.js";
+import path from "path";
 import "./passport/github.auth.js";
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
+console.log("dirname", __dirname);
 
 app.use(
   session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
@@ -26,7 +31,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/explore", exploreRoutes);
 
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on port http://localhost:${PORT}`);
   connectMongoDB();
 });
